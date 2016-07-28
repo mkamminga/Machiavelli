79ea4548a8f65d4e35077d64d91a323afeaee408
mjkamminga@gmail.com <mjkamminga@gmail.com>
2016-07-28 19:09:49 +0200

    Initial Commit

---
+++ Machiavelli/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Machiavelli
+//
+//  Created by Mark Jan Kamminga on 28-07-16.
+//  Copyright © 2016 Mark Jan Kamminga. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
