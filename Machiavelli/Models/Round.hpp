c9f37e11fa90f1fbc7d177bf2e0a6ea94ecafc09
mjkamminga@gmail.com <mjkamminga@gmail.com>
2016-08-10 20:50:33 +0200

    ROUND updates

--- Machiavelli/Models/Round.hpp
+++ Machiavelli/Models/Round.hpp
@@ -12,12 +12,23 @@
 #include <stdio.h>
 
 #include "BaseCharacter.hpp"
+#include "Game.hpp"
 
 class Round {
 public:
-    
+    Round(std::shared_ptr<Game> game) : game(game) {
+        characters[0] = MURDERER;
+        characters[1] = THIEF;
+        characters[2] = WIZZARD;
+        characters[3] = PREACHER;
+        characters[4] = MERCHANT;
+        characters[5] = MASTER_BUILER;
+        characters[7] = CONDOTTIERI;
+        
+    };
 private:
-    
+    std::shared_ptr<Game> game;
+    int characters[7];
 };
 
 #endif /* Round_hpp */
